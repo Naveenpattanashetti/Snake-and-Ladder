d554f689e004c93d835d59c55260d15472f541f0
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-29 18:51:01 +0530

    welcome to snake and ladder program

---
+++ Snake_and_ladder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snake_and_ladder;
+
+public class Snake_and_ladder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to snake and  ladder program");
+
+	}
+
+}
