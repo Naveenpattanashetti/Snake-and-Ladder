042fda0d22713a5516695b38b0804ad7dc5d64b3
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-29 19:21:20 +0530

    Die_To_Get_A_Number_Btwn_1 To 6

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -2,9 +2,18 @@ package com.bridgelabz.snake_and_ladder;
 
 public class Snake_and_ladder {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		int Position = 0;
 		System.out.println("Welcome into Snake and Ladder program !!");
-		System.out.println("Your Starting Position is : " + Position);}
+		System.out.println("Your Starting Position is : " + Position);
+		Dice();
+	}
+	public static void Dice(){
+		int Dice = (int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Your Dice Value is : " +Dice);
+	}
 
 }
+		
+
+
